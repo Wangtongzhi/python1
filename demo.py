b0bb4475c64980bd304c6536ea11b66f69fc4e9a
Wangtongzhi <wtz960103@163.com>
2020-05-15 09:49:29 +0800

    22222222

--- demo.py
+++ demo.py
@@ -21,4 +21,28 @@ a = "张三"  # 把张三赋给了a
 # print ("相加为：",a+b) '''
 a = input("请输入一段内容")
 b = input("请输入另一段内容")
+print("长度为：", len(a + b))
+"""print("hello python!")  #字符串
+print(1111)#整数
+print(1.2)#小数
+print(True)#布尔值
+print(())#元组
+print({})#字典
+print([])  #数组
+"""
+"""
+print("硕士生", 22, 222)
+print(5 + 6)
+print("hah" * 4)
+"""
+"""
+a = "张三"  # 把张三赋给了a
+"""
+
+# ''' 
+# a = float(input("请输入ling一个数"))
+# b = float (input("请输入一个数"))
+# print ("相加为：",a+b) '''
+a = input("请输入一段内容")
+b = input("请输入另一段内容")
 print("长度为：",len(a+b))
