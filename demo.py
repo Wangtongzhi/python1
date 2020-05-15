5f850483ad48cf8bb34aa9b6b5872a091578642f
Wangtongzhi <wtz960103@163.com>
2020-05-15 10:41:05 +0800

    333

--- demo.py
+++ demo.py
@@ -1,48 +1,57 @@
-"""print("hello python!")  #字符串
-print(1111)#整数
-print(1.2)#小数
-print(True)#布尔值
-print(())#元组
-print({})#字典
-print([])  #数组
-"""
-"""
-print("硕士生", 22, 222)
-print(5 + 6)
-print("hah" * 4)
-"""
-"""
-a = "张三"  # 把张三赋给了a
-"""
+# """print("hello python!")  #字符串
+# print(1111)#整数
+# print(1.2)#小数
+# print(True)#布尔值
+# print(())#元组
+# print({})#字典
+# print([])  #数组
+# """
+# """
+# print("硕士生", 22, 222)
+# print(5 + 6)
+# print("hah" * 4)
+# """
+# """
+# a = "张三"  # 把张三赋给了a
+# """
 
-# ''' 
-# a = float(input("请输入ling一个数"))
-# b = float (input("请输入一个数"))
-# print ("相加为：",a+b) '''
-a = input("请输入一段内容")
-b = input("请输入另一段内容")
-print("长度为：", len(a + b))
-"""print("hello python!")  #字符串
-print(1111)#整数
-print(1.2)#小数
-print(True)#布尔值
-print(())#元组
-print({})#字典
-print([])  #数组
-"""
-"""
-print("硕士生", 22, 222)
-print(5 + 6)
-print("hah" * 4)
-"""
-"""
-a = "张三"  # 把张三赋给了a
-"""
+# # ''' 
+# # a = float(input("请输入ling一个数"))
+# # b = float (input("请输入一个数"))
+# # print ("相加为：",a+b) '''
+# a = input("请输入一段内容")
+# b = input("请输入另一段内容")
+# print("长度为：", len(a + b))
+# """print("hello python!")  #字符串
+# print(1111)#整数
+# print(1.2)#小数
+# print(True)#布尔值
+# print(())#元组
+# print({})#字典
+# print([])  #数组
+# """
+# """
+# print("硕士生", 22, 222)
+# print(5 + 6)
+# print("hah" * 4)
+# """
+# """
+# a = "张三"  # 把张三赋给了a
+# """
 
-# ''' 
-# a = float(input("请输入ling一个数"))
-# b = float (input("请输入一个数"))
-# print ("相加为：",a+b) '''
-a = input("请输入一段内容")
-b = input("请输入另一段内容")
-print("长度为：",len(a+b))
+# # ''' 
+# # a = float(input("请输入ling一个数"))
+# # b = float (input("请输入一个数"))
+# # print ("相加为：",a+b) '''
+# a = input("请输入一段内容")
+# b = input("请输入另一段内容")
+# print("长度为：",len(a+b))
+#元组
+# a = (1, 2, 3, 4, "哈哈哈", True)
+# print(a.count("哈哈哈"))
+
+a = input("请输入名字： ")
+b = input("请输入年龄： ")
+c = input("性别： ")
+d = {"name":a, "age":b, "sex":c}
+print(d)
---
+++ demo02.py
@@ -0,0 +1,8 @@
+#判断
+a = 1
+b = 2
+if a > b:
+    print("111")
+else:
+    print("222")
+    
