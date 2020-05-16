81aa00ebf41538acbae4a0cf8724914e9ea4bbcc
Wangtongzhi <wtz960103@163.com>
2020-05-16 15:34:33 +0800

    0516

---
+++ apple.py
@@ -0,0 +1,3 @@
+price=input("请输入苹果单价：")
+weight=input("请输入苹果重量： ")
+print("苹果总价为： ",int(price)*int(weight)-5)
--- demo02.py
+++ demo02.py
@@ -1,8 +1,31 @@
-#判断
+# #判断
+# # a = 1
+# # b = 2
+# # if a > b:
+# #     print("111")
+# # else:
+# #     print("222")
+# high = {}
+# low = {}
+# list = ["a", "b", "c", "d", "e", "f", "g"]
+# count = 0
+# while count < len(list):
+#     score = int(input("请输入" + list[count] + "成绩"))
+#     if score > 60:
+#         high[list[count]]=score
+#     else:
+#         low[list[count]]=score
+#     count = count + 1
+# print(high)
+# for i in range(1,10):
+#     for j in range(1,i + 1):
+#         print(i, "X", j, "=", i * j, end="  ")
+#     print()
 a = 1
-b = 2
-if a > b:
-    print("111")
-else:
-    print("222")
-    
+while a>0:
+    for i in range(1,31):
+        print(i)
+    for j in range(1,36):
+        print(j)
+    for k in range(1,4):
+        print(k)
